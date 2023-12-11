da8c37d15501dfec11e5af5405e7be04d957ef92
Sivaprasath <121082414+sivaprasath2004@users.noreply.github.com>
2023-12-11 12:52:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 const express=require('express')
 const app=express()
-const cors=require('cors')
 const path = require('path');
-const { engine } = require('express-handlebars')
-app.use(express.static(path.join(__dirname,'public')))
-app.use(cors())
-app.engine('hbs',engine({ extname: '.hbs', defaultLayout: false }))
-
-app.set('view engine', 'hbs')
-const PORT=process.env.PORT || 5000;
-const {login}=require('./controller//loginControl')
-const signUp=require('./controller/signupControl');
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use("/",require('./routes/main'))
-app.use('/',express.static(path.join(__dirname,'public')))
-app.post("/login",login)
-app.post("/signup",signUp)
-app.listen(PORT,()=>console.log("port is:",PORT))
+app.get('/',((req,res)=>{
+  res.send("hello")}))
+app.listen(5000)
 
